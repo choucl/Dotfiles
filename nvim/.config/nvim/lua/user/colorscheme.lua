a70579627fcb76b7118331be049072178f1cf088
choucl <clc25600@gmail.com>
2022-03-26 21:27:00 +0800

    [nvim] Change nightfox configs to fix deprecate warnings

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -3,20 +3,22 @@ local nightfox = require('nightfox')
 -- This function set the configuration of nightfox. If a value is not passed in the setup function
 -- it will be taken from the default configuration above
 nightfox.setup({
-  fox = "nordfox", -- change the colorscheme to use nordfox
-  styles = {
-    comments = "italic", -- change style of comments to be italic
-    keywords = "bold", -- change style of keywords to be bold
-    functions = "bold" -- styles can be a comma separated list
+  -- fox = "nordfox", -- change the colorscheme to use nordfox
+  options = {
+    styles = {
+      comments = "italic", -- change style of comments to be italic
+      keywords = "bold", -- change style of keywords to be bold
+      functions = "bold" -- styles can be a comma separated list
+    },
+    inverse = {
+      match_paren = true, -- inverse the highlighting of match_parens
+    }
   },
-  inverse = {
-    match_paren = true, -- inverse the highlighting of match_parens
-  },
-  colors = {
+  pallets = {
     -- red = "#FF000", -- Override the red color for MAX POWER
     -- bg_alt = "#000000",
   },
-  hlgroups = {
+  groups = {
     type = { fg = "#88C0D0" }
   }
 })
