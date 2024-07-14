a4147b18c9c07b46e09b11356feb10d807404a49
choucl <n28111542@gs.ncku.edu.tw>
2024-07-14 14:03:16 +0800

    Add bsv highlighting

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
@@ -7,9 +5,24 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
+    
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    parser_config.bsv = {
+      install_info = {
+        url = "https://github.com/yuyuranium/tree-sitter-bsv", -- local path or git repo
+        files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+        -- optional entries:
+        branch = "main", -- default branch in case of git repo if different from master
+        generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+        requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+      },
+      filetype = "bsv", -- if filetype does not match the parser name
+    }
+
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       "lua",
       "vim",
+      "bsv"
       -- add more arguments for adding more treesitter parsers
     })
   end,
